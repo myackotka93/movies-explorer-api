5a2395eb718428b3f64a63e80b7ad29a8161955f
Valeriya Kuznetsova <88514633+myackotka93@users.noreply.github.com>
2022-04-29 01:10:47 +0300

    Revert "Level 1"

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const users = require('express').Router();
-const { updateUserInfo, getUserInfo } = require('../controllers/users');
-const { userInfoValidation } = require('../middlewares/validation');
-
-users.get('/users/me', getUserInfo);
-users.patch('/users/me', userInfoValidation, updateUserInfo);
-
-module.exports = users;
